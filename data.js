1f251359605caf9c2ce82156e6bb5e6094314458
saoocthe02 <108472891+saoocthe02@users.noreply.github.com>
2025-06-30 10:18:21 +0700

    /js

---
+++ data.js
@@ -0,0 +1,37 @@
+// Dữ liệu mod skin
+const modData = [
+    {
+        champion: "Yasuo",
+        skins: [
+            {
+                name: "Yasuo Ma Kiếm",
+                icon: "data/Yasuo/Yasuo Ma Kiếm/icon.png",
+                splash: "data/Yasuo/Yasuo Ma Kiếm/splash.png",
+                zip: "data/Yasuo/Yasuo Ma Kiếm/Yasuo Ma Kiếm.zip"
+            },
+            {
+                name: "Yasuo Huyết Nguyệt",
+                icon: "data/Yasuo/Yasuo Huyết Nguyệt/icon.png",
+                splash: "data/Yasuo/Yasuo Huyết Nguyệt/splash.png",
+                zip: "data/Yasuo/Yasuo Huyết Nguyệt/Yasuo Huyết Nguyệt.zip"
+            }
+        ]
+    },
+    {
+        champion: "Zed",
+        skins: [
+            {
+                name: "Zed Hắc Tinh",
+                icon: "data/Zed/Zed Hắc Tinh/icon.png",
+                splash: "data/Zed/Zed Hắc Tinh/splash.png",
+                zip: "data/Zed/Zed Hắc Tinh/Zed Hắc Tinh.zip"
+            },
+            {
+                name: "Zed Chúa Tể",
+                icon: "data/Zed/Zed Chúa Tể/icon.png",
+                splash: "data/Zed/Zed Chúa Tể/splash.png",
+                zip: "data/Zed/Zed Chúa Tể/Zed Chúa Tể.zip"
+            }
+        ]
+    }
+];
